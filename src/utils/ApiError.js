813529cc8f39d698702fb56a90a9c78b4761dfda
Rounak Mitra <rounakmitra9@gmail.com>
2025-12-15 00:08:53 +0530

    Add ApiError, ApiResponse, and asyncHandler utility classes

---
+++ src/utils/ApiError.js
@@ -0,0 +1,17 @@
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        messsage = `Error happened in ApiError`,
+        errors = [],
+        stack = ""
+    ){
+        super(messsage,)
+        this.statusCode = statusCode
+        this.data = null
+        this.success = false
+        this.message = messsage
+        this.errors = errors
+    }
+}
+
+module.exports = ApiError
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,14 @@
+const asyncHandler = (fn)=> async (req,res,next)=>{
+    try{
+        await fn(req,res,next)
+    }catch(err){
+        res
+        .status(err.code || 500)
+        .json({
+            success:false,
+            error:err.message || `Internal server error :(`
+        })
+    }
+}
+
+module.exports = asyncHandler
